2c2384271a47c2549c9002a711a7e7f54b6ce219
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-11-17 23:47:55 +0530

    added install scripts

---
+++ arch.sh
@@ -0,0 +1,10 @@
+sudo pacman -S virt-manager qemu qemu-arch-extra ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat  --needed --noconfirm
+
+
+sudo systemctl enable libvirtd.service
+
+
+sudo systemctl start libvirtd.service
+
+
+
---
+++ debian.sh
@@ -0,0 +1,6 @@
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager -yy
+
+sudo adduser $USER libvirt
+
+sudo adduser $USER kvm
+
