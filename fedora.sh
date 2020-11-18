dce7a58eab16fad9e43b33586544ed334b76c518
nebulaxyz <62870678+nebulaxyz@users.noreply.github.com>
2020-11-18 11:27:59 +0530

    added f- install scripts

---
+++ fedora.sh
@@ -0,0 +1,12 @@
+sudo dnf -y install bridge-utils libvirt virt-install qemu-kvm virt-top libguestfs-tools virt-manager
+
+sudo systemctl start libvirtd
+
+sudo systemctl enable libvirtd
+
+
+
+
+
+
+
